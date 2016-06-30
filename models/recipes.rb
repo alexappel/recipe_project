524ac109a77644e06b132eae558d959ab75ef413
Jolie Lerner <jolie.alyse@gmail.com>
2016-06-30 17:14:49 +0000

    recipe file worked on

--- models/recipes.rb
+++ models/recipes.rb
@@ -5,61 +5,61 @@ end
 
 @image = "images/imgres-1.jpg"
 
-pancakes = "Prep time: 5 mins
-Cook time: 10 mins
-Ready in: 15 mins
-Ingredients:
-1 cup all-purpose flour
-1 tablespoon white sugar
-2 teaspoons baking powder
-1/4 teaspoon salt
-1 egg, beaten
-1 cup milk
-2 tablespoons vegetable oil
-2 ripe bananas, mashed
-Directions:
-Combine flour, white sugar, baking powder and salt. In a separate bowl, mix together egg, milk, vegetable oil and bananas.
-Stir flour mixture into banana mixture; batter will be slightly lumpy.
-Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Cook until pancakes are golden brown on both sides; serve hot"
-
-grilledcheese = "Prep time: 5 mins
-Cook time:  10 mins
-Ready in: 15 mins
-Ingredients:
-2 slices Italian bread
-1 tablespoon softened butter, divided
-1 tablespoon prepared pesto sauce, divided
-1 slice provolone cheese
-2 slices tomato
-1 slice American cheese
-Directions:
-Spread one side of a slice of bread with butter, and place it, buttered side down, into a nonstick skillet over medium heat.
-Spread the top of the bread slice in the skillet with half the pesto sauce, and place a slice of provolone cheese, the tomato slices, and the slice of American cheese onto the pesto.
-Spread remaining pesto sauce on one side of the second slice of bread, and place the bread slice, pesto side down, onto the sandwich. Butter the top side of the sandwich.
-Gently fry the sandwich, flipping once, until both sides of the bread are golden brown and the cheese has melted, about 5 minutes per side."
-
-pasta = "INGREDIENTS:
-Servings: 4
-8 oz linguine
-2 Tbsp. olive oil
-8 Tbsp. (1 stick) unsalted butter
-4 cloves garlic, minced
-1 tsp crushed red pepper
-1¼ lbs large shrimp
-Salt and pepper to taste
-1 tsp dried oregano
-4 cups baby spinach
-¼ cup Parmesan
-2 Tbsp. parsley, chopped
-1 Tbsp. lemon juice
-PREP:
-1. In a large pot, boil water and add pasta. Cook (stirring frequently) until al dente.
-2. Drain and set aside pasta.
-3. In the same pan, heat olive oil and 2 tablespoons of butter. Add garlic and crushed red pepper, cook until fragrant.
-4. Toss in shrimp, salt and pepper to taste, and stir until shrimp start to turn pink, but are not fully cooked.
-5. Add oregano and spinach, cook until wilted.
-6. Return cooked pasta to the pot, add remaining butter, parmesan, and parsley. Stir until well mixed and the butter is melted.
-7. When the shrimp are cooked, add lemon juice, mix once more, then serve while hot.
-8. Enjoy!"
+# pancakes = "Prep time: 5 mins
+# Cook time: 10 mins
+# Ready in: 15 mins
+# Ingredients:
+# 1 cup all-purpose flour
+# 1 tablespoon white sugar
+# 2 teaspoons baking powder
+# 1/4 teaspoon salt
+# 1 egg, beaten
+# 1 cup milk
+# 2 tablespoons vegetable oil
+# 2 ripe bananas, mashed
+# Directions:
+# Combine flour, white sugar, baking powder and salt. In a separate bowl, mix together egg, milk, vegetable oil and bananas.
+# Stir flour mixture into banana mixture; batter will be slightly lumpy.
+# Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Cook until pancakes are golden brown on both sides; serve hot"
+#
+# grilledcheese = "Prep time: 5 mins
+# Cook time:  10 mins
+# Ready in: 15 mins
+# Ingredients:
+# 2 slices Italian bread
+# 1 tablespoon softened butter, divided
+# 1 tablespoon prepared pesto sauce, divided
+# 1 slice provolone cheese
+# 2 slices tomato
+# 1 slice American cheese
+# Directions:
+# Spread one side of a slice of bread with butter, and place it, buttered side down, into a nonstick skillet over medium heat.
+# Spread the top of the bread slice in the skillet with half the pesto sauce, and place a slice of provolone cheese, the tomato slices, and the slice of American cheese onto the pesto.
+# Spread remaining pesto sauce on one side of the second slice of bread, and place the bread slice, pesto side down, onto the sandwich. Butter the top side of the sandwich.
+# Gently fry the sandwich, flipping once, until both sides of the bread are golden brown and the cheese has melted, about 5 minutes per side."
+#
+# pasta = "INGREDIENTS:
+# Servings: 4
+# 8 oz linguine
+# 2 Tbsp. olive oil
+# 8 Tbsp. (1 stick) unsalted butter
+# 4 cloves garlic, minced
+# 1 tsp crushed red pepper
+# 1¼ lbs large shrimp
+# Salt and pepper to taste
+# 1 tsp dried oregano
+# 4 cups baby spinach
+# ¼ cup Parmesan
+# 2 Tbsp. parsley, chopped
+# 1 Tbsp. lemon juice
+# PREP:
+# 1. In a large pot, boil water and add pasta. Cook (stirring frequently) until al dente.
+# 2. Drain and set aside pasta.
+# 3. In the same pan, heat olive oil and 2 tablespoons of butter. Add garlic and crushed red pepper, cook until fragrant.
+# 4. Toss in shrimp, salt and pepper to taste, and stir until shrimp start to turn pink, but are not fully cooked.
+# 5. Add oregano and spinach, cook until wilted.
+# 6. Return cooked pasta to the pot, add remaining butter, parmesan, and parsley. Stir until well mixed and the butter is melted.
+# 7. When the shrimp are cooked, add lemon juice, mix once more, then serve while hot.
+# 8. Enjoy!"
 
 recipes(pancakes, grilledcheese, pasta)
