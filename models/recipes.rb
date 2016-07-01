fabf715d5761c34cd1c59d5152fcdbdf63451d6e
Alex Appel <alexappel@Alexs-MacBook-Pro.local>
2016-07-01 13:44:35 -0400

    fixed all files

--- models/recipes.rb
+++ models/recipes.rb
@@ -12,10 +12,10 @@
 
 def recipes(meal, difficulty)
   recipes = {
-    :breakfast => [{:name=>"Oatmeal", :imgaddress=>"images/oatmeal_recipe.png"}, {:name=>"Pancakes", :imgaddress=>"images/pancake_recipe.png"}, {:name=>"Fritatta", :imgaddress=> "images/fritatta_recipe.png"}],
-    :lunch => [{:name=>"Grilled Cheese", :imgaddress=>"images/grilled_cheese.png"}, {:name=>"Salad", :imgaddress=>"images/salad_recipe.png"}, {:name=>"Turkey Burger", :imgaddress=> "images/turkeyburger_recipe.png"}],
-    :dinner => [{:name=>"Tomato Soup", :imgaddress=>"images/tomato_soup.png"}, {:name=>"Pasta", :imgaddress=>"images/pasta_recipe.png"}, {:name=>"Chicken", :imgaddress=>"images/chicken_recipe.png"}],
-    :dessert => [{:name=>"Ice Cream Sandwich", :imgaddress=>"images/ice_cream_sandwich_recipe.png"}, {:name=>"Flourless Chocolate Cake", :imgaddress=>"images/flourless_chocolate_cake_recipe.png"}, {:name=>"Macarron", :imgaddress=>"images/macarron_recipe.png"}]
+    :breakfast => [{:name=>"oatmeal", :imgaddress=>"images/oatmeal_recipe.png"}, {:name=>"banana pancakes", :imgaddress=>"images/pancake_recipe.png"}, {:name=>"a fritatta", :imgaddress=> "images/fritatta_recipe.png"}],
+    :lunch => [{:name=>"grilled cheese", :imgaddress=>"images/grilled_cheese.png"}, {:name=>"a salad", :imgaddress=>"images/salad_recipe.png"}, {:name=>"a turkey burger", :imgaddress=> "images/turkey_burger.png"}],
+    :dinner => [{:name=>"tomato soup", :imgaddress=>"images/tomato_soup.png"}, {:name=>"garlic lemon shrimp pasta", :imgaddress=>"images/pasta_recipe.png"}, {:name=>"tangine chicken", :imgaddress=>"images/chicken_recipe.png"}],
+    :dessert => [{:name=>"ice cream sandwiches", :imgaddress=>"images/ice_cream_sandwich_recipe.png"}, {:name=>"flourless chocolate cake", :imgaddress=>"images/flourless_chocolate_cake_recipe.png"}, {:name=>"macarrons", :imgaddress=>"images/macarron_recipe.png"}]
   }
     puts recipes[meal][difficulty]
     recipes[meal][difficulty]
