e3d9f32d0b54df4eb922b611d71d9e4a978bc582
Jolie Lerner <jolie.alyse@gmail.com>
2016-07-01 17:21:54 +0000

    recipe.rb dessert

--- models/recipes.rb
+++ models/recipes.rb
@@ -14,7 +14,8 @@ def recipes(meal, difficulty)
   recipes = {
     :breakfast => [{:name=>"Oatmeal", :imgaddress=>"images/oatmeal_recipe.png"}, {:name=>"Pancakes", :imgaddress=>"images/pancake_recipe.png"}, {:name=>"Fritatta", :imgaddress=> "images/fritatta_recipe.png"}],
     :lunch => [{:name=>"Grilled Cheese", :imgaddress=>"images/grilled_cheese.png"}, {:name=>"Salad", :imgaddress=>"images/salad_recipe.png"}, {:name=>"Turkey Burger", :imgaddress=> "images/turkeyburger_recipe.png"}],
-    :dinner => [{:name=>"Tomato Soup", :imgaddress=>"images/tomato_soup.png"}, {:name=>"Pasta", :imgaddress=>"images/pasta_recipe.png"}, {:name=>"Chicken", :imgaddress=>"images/chicken_recipe.png"}]
+    :dinner => [{:name=>"Tomato Soup", :imgaddress=>"images/tomato_soup.png"}, {:name=>"Pasta", :imgaddress=>"images/pasta_recipe.png"}, {:name=>"Chicken", :imgaddress=>"images/chicken_recipe.png"}],
+    :dessert => [{:name=>"Ice Cream Sandwich", :imgaddress=>"images/ice_cream_sandwich_recipe.png"}, {:name=>"Flourless Chocolate Cake", :imgaddress=>"images/flourless_chocolate_cake_recipe.png"}, {:name=>"Macarron", :imgaddress=>"images/macarron_recipe.png"}]
   }
     puts recipes[meal][difficulty]
     recipes[meal][difficulty]
